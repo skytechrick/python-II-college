2df82161f7287d193430721dc5cd11fcc827872a
= <ricksarkar2005@gmail.com>
2025-03-04 20:54:28 +0530

    q4.1 is done

---
+++ lab1/q4.1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x = [1, 4, 66, 2]
+
+maximum = np.max(x)
+minimum = np.min(x)
+range = maximum - minimum
+
+print("Max:", maximum)
+print("Min:", minimum)
+print("Range:", range)
