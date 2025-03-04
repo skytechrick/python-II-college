eb5ebb81edc9c7b29de663dc85106018964d643b
= <ricksarkar2005@gmail.com>
2025-03-05 01:03:33 +0530

    q6.3 done

---
+++ lab1/q6.3.py
@@ -0,0 +1,8 @@
+import statistics as st
+import random as r
+
+n = int(input("Enter the number of elements: "))
+x = []
+for i in range(n):
+    x.append(r.randint(1, 100))
+print("Variance of x:", st.stdev(x))
